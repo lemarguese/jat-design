6e9717cf48bc1b773343c7b3fc33adcd8cc45038
Le Marguesse <amargesono@gmail.com>
2022-05-24 19:12:13 +0600

    jat init

---
+++ index.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    let hseRoute = document.getElementsByClassName("jat-routing_text hse")[0];
+
+    hseRoute.addEventListener('click', function () {
+        window.location.assign("hse.html");
+    })
+}
